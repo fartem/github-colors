fb96a0217e6cb7b8d469b7bac0c89a68322463fe
fartem <jaman.smlnsk@gmail.com>
2020-05-10 07:36:17 +0300

    2020-05-10 Update: Fixed Rubocop issues

--- parser.rb
+++ parser.rb
@@ -1,27 +1,24 @@
+#!/usr/bin/ruby
+
 require 'yaml'
 require 'json'
 require 'csv'
 require 'open-uri'
 
-languages_raw = open('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml') { |f|
-    f.read
-}
-languages_data = YAML::load(languages_raw)
+colors_url = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
+languages_raw = URI.open(colors_url, &:read)
+languages_data = YAML.safe_load(languages_raw)
 
 languages = {}
-languages_data.each { |language|
-    color = language[1]['color']
-    if !color.nil?
-        languages[language[0]] = color
-    end
-}
+languages_data.each do |language|
+  color = language[1]['color']
+  languages[language[0]] = color unless color.ni?
+end
 
 File.open('./colors.json', 'w') do |f|
-    f.write(JSON.pretty_generate(languages))
+  f.write(JSON.pretty_generate(languages))
 end
 
 CSV.open('./colors.cvs', 'w') do |f|
-    languages.to_a.each { |language|
-        f << language
-    }
+  languages.to_a.each { |language| f << language }
 end
