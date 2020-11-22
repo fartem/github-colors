5124ce6f508e5a325128258886e2f44546a2089a
fartem <jaman.smlnsk@gmail.com>
2020-11-22 08:18:54 +0300

    2020-11-22 Update: Fixed Rubocop issues and updated colors

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ GitHub languages colors parser.
 
 ## How to run Parser
 
-Install [Git](https://git-scm.com/) and [Ruby](https://www.ruby-lang.org/en/). Then run from shell:
+Install [Git](https://git-scm.com) and [Ruby](https://www.ruby-lang.org/en). Then run from shell:
 
 ```shell
 $ git clone https://github.com/fartem/github-colors.git
--- parser.rb
+++ parser.rb
@@ -6,13 +6,13 @@ require 'csv'
 require 'open-uri'
 
 colors_url = 'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml'
-languages_raw = URI.open(colors_url, &:read)
+languages_raw = URI.parse(colors_url, &:read).open
 languages_data = YAML.safe_load(languages_raw)
 
 languages = {}
 languages_data.each do |language|
   color = language[1]['color']
-  languages[language[0]] = color unless color.ni?
+  languages[language[0]] = color unless color.nil?
 end
 
 File.open('./colors.json', 'w') do |f|
